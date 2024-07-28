6940e617bf1ae95df8e9923996ed1c9dca434f1e
lukemoody <luke@thirty3digital.co.uk>
2024-07-28 20:27:27 +0100

    pdated logic for provider access

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,6 +1,9 @@
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 import { NextAuthOptions } from "next-auth";
+import { db } from "@/types/db";
+import bcrypt from "bcryptjs";
 
 // SETUP AUTH PROVIDERS
 // https://next-auth.js.org/configuration/providers/oauth
@@ -21,8 +24,7 @@ const options: NextAuthOptions = {
     }),
     GoogleProvider({
       profile(profile) {
-        // console.log("Profile Google", profile);
-        let userRole = "Google User";
+        let userRole = "USER";
         if (profile?.email == "luke@thirty3digital.co.uk") {
           userRole = "ADMIN";
         }
@@ -35,6 +37,48 @@ const options: NextAuthOptions = {
       },
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    // Provider setup for custom user login
+    CredentialsProvider({
+      id: "password",
+      name: "Username and Password",
+      // The below credentials setups the login form with specific fields.
+      // FOr the example belwo it creates a form with username and password fields.
+      credentials: {
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "Username...",
+        },
+        password: { label: "Password", type: "Password" },
+      },
+      authorize: async (credentials) => {
+        if (!credentials) {
+          return null;
+        }
+
+        // Check the DB for existing user data
+        const user = await db.user.findUnique({
+          where: { username: credentials.username },
+        });
+
+        if (!user) {
+          return null;
+        }
+
+        // Use bcrypt compare the saved hashed user password with the password
+        // they have just entered.
+        const match = await bcrypt.compare(
+          credentials!.password,
+          user.password
+        );
+
+        if (match) {
+          return user;
+        }
+
+        return null;
+      },
     }),
   ],
   // ADD CALLBACKS
